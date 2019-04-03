9bc5171f266ae9331805a5447c9d528f23bc70bd
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-03 12:57:02 -0700

    Refactoring to Node/Express/React app boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    data: null
-  };
-
-  componentDidMount() {
-    // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // Fetches our GET route from the Express server
-    callBackendAPI = async () => {
-      const response = await fetch('/express_backend');
-      const body = await response.json();
-
-      if (response.status !== 200) {
-        throw Error(body.message)
-      }
-      return body;
-    };
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {/* Render the newly fetched data inside of this.state.data */}
-        <p className="App-intro">{this.state.data}</p>
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
