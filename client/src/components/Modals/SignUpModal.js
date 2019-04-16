ab1eb31101f866d58d13a8a533b489e786e2ca4d
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-16 13:22:11 -0700

    Connecting SignUpModal component to Redux store

--- client/src/components/Modals/SignUpModal.js
+++ client/src/components/Modals/SignUpModal.js
@@ -1,7 +1,13 @@
 import React from 'react';
+// This was in the tutorial from part 1 - not sure why we didn't include it or if we need it. - BG
+// import { Link, withRouter } from "react-router-dom";
 import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import SignUpForm from '../forms/SignUpForm';
 import axios from 'axios';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
 
 class SignUpModal extends React.Component {
   constructor(props) {
@@ -18,7 +24,13 @@ class SignUpModal extends React.Component {
     this.toggle = this.toggle.bind(this);
   }
   
-
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
   
   toggle() {
     this.setState(prevState => ({
@@ -37,6 +49,7 @@ class SignUpModal extends React.Component {
       password2: this.state.password2
     }).then(res => {
       console.log(res)
+      this.props.registerUser(newUser, this.props.history);
     })
   } else {alert("Passwords must match")}
   };
@@ -51,18 +64,42 @@ class SignUpModal extends React.Component {
   render() {
     return (
       <div>
-        <button className="btn" onClick={this.toggle}>Not a user? Sign Up</button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Sign Up</ModalHeader>
-          <ModalBody>
-            <SignUpForm handleFormSubmit={this.createUser} handleInputChange={this.handleInputChange} />
-          </ModalBody>
+        <button 
+          className="btn" 
+          onClick={this.toggle}
+          >Not a user? Sign Up
+        </button>
+        <Modal 
+          isOpen={this.state.modal} 
+          toggle={this.toggle} 
+          className={this.props.className}>
+            <ModalHeader 
+              toggle={this.toggle}>Sign Up
+            </ModalHeader>
+            <ModalBody>
+              <SignUpForm 
+                handleFormSubmit={this.createUser} 
+                handleInputChange={this.handleInputChange} 
+              />
+            </ModalBody>
         </Modal>
       </div>
     );
   }
 }
 
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
 
-export default SignUpModal;
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(SignUpModal));
