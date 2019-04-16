8ed8813ade4c9c83f395bf8ea6ed3b54e9457bbe
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-16 13:17:15 -0700

    Edited formatting for easier reading

--- client/src/components/forms/SignUpForm.js
+++ client/src/components/forms/SignUpForm.js
@@ -6,19 +6,43 @@ function SignUpForm (props) {
       <Form>
         <FormGroup>
           <Label for="name">Name</Label>
-          <Input value={props.name} onChange={props.handleInputChange} type="name" name="name" id="userName" placeholder="name" />
+          <Input 
+            value={props.name} 
+            onChange={props.handleInputChange} 
+            type="name" 
+            name="name" 
+            id="userName" 
+            placeholder="name" />
         </FormGroup>
         <FormGroup>
           <Label for="email">Email</Label>
-          <Input value={props.email} onChange={props.handleInputChange} type="email" name="email" id="userEmail" placeholder="email" />
+          <Input 
+            value={props.email} 
+            onChange={props.handleInputChange} 
+            type="email" 
+            name="email" 
+            id="userEmail" 
+            placeholder="email" />
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
-          <Input value={props.password} onChange={props.handleInputChange} type="password" name="password" id="userPassword" placeholder="password" />
+          <Input 
+            value={props.password} 
+            onChange={props.handleInputChange} 
+            type="password" 
+            name="password" 
+            id="userPassword" 
+            placeholder="password" />
         </FormGroup>
         <FormGroup>
           <Label for="confPassword">Confirm Password</Label>
-          <Input value={props.password2} onChange={props.handleInputChange} type="password" name="password2" id="userConfPassword" placeholder="confim password" />
+          <Input 
+            value={props.password2} 
+            onChange={props.handleInputChange} 
+            type="password" 
+            name="password2" 
+            id="userConfPassword" 
+            placeholder="confim password" />
         </FormGroup>
         <Button onClick={props.handleFormSubmit}>Submit</Button>
       </Form>
