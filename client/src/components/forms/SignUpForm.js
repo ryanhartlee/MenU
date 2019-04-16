3a27ece772fa5a7e4ef1ae2ba9c6ea0a97cc8cb7
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-16 13:38:19 -0700

    Working on displaying errors in form

--- client/src/components/forms/SignUpForm.js
+++ client/src/components/forms/SignUpForm.js
@@ -6,43 +6,68 @@ function SignUpForm (props) {
       <Form>
         <FormGroup>
           <Label for="name">Name</Label>
+          <span className="red-text">{errors.name}</span>
           <Input 
             value={props.name} 
             onChange={props.handleInputChange} 
             type="name" 
             name="name" 
             id="userName" 
-            placeholder="name" />
+            placeholder="name"
+            error={errors.name}
+            className={classnames("", {
+              invalid: errors.name
+            })}
+          />
         </FormGroup>
         <FormGroup>
           <Label for="email">Email</Label>
+          <span className="red-text">{errors.email}</span>
           <Input 
             value={props.email} 
             onChange={props.handleInputChange} 
             type="email" 
             name="email" 
             id="userEmail" 
-            placeholder="email" />
+            placeholder="email"
+            error={errors.email}
+            className={classnames("", {
+              invalid: errors.email
+            })} 
+          />
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
+          <span className="red-text">{errors.password}</span>
           <Input 
             value={props.password} 
             onChange={props.handleInputChange} 
             type="password" 
             name="password" 
             id="userPassword" 
-            placeholder="password" />
+            placeholder="password" 
+            error={errors.password}
+            className={classnames("", {
+              invalid: errors.password
+            })}
+          />
         </FormGroup>
         <FormGroup>
           <Label for="confPassword">Confirm Password</Label>
+          <span className="red-text">{errors.password2}</span>
           <Input 
             value={props.password2} 
             onChange={props.handleInputChange} 
             type="password" 
             name="password2" 
+            // Change this ID to password2?
             id="userConfPassword" 
-            placeholder="confim password" />
+            placeholder="confim password" 
+            error={errors.password2}
+            className={classnames("", {
+              invalid: errors.password2
+            })}
+          />
         </FormGroup>
         <Button onClick={props.handleFormSubmit}>Submit</Button>
       </Form>
