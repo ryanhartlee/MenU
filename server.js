cfee5c0ee3115bb581f58b8cc935425f72cb8b26
Patrycja Gorczynska <patrycjagorczynska@patrycjas-mbp.lan>
2019-04-20 14:47:42 -0700

    Path

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const path = require('path');
 // const db = require('./models');
 const app = express();
-const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const passport = require("passport");
 const users = require("./routes/api/users");
