887fb13bad22dfafa6aeca7f096b0c6568da5ef6
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-25 21:14:06 -0700

    Made changes to test Heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     mongoURI: process.env.MONGODB_URI || "mongodb://localhost/menudb" 
   };
 
-module.exports = {
-    mongoURI: "mongodb://testuser:testuser1@ds239206.mlab.com:39206/heroku_g6wz19x7",
-    secretOrKey: "secret"
-  };
+// module.exports = {
+//     mongoURI: "mongodb://testuser:testuser1@ds239206.mlab.com:39206/heroku_g6wz19x7",
+//     secretOrKey: "secret"
+//   };
