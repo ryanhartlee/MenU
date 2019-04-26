c38514cfe64739888a76d47d3356aef5396f37f3
Ryan Hartley <ryanhartlee@gmail.com>
2019-04-26 14:49:11 -0700

    heroku fix?

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 
-// const db = require('./models');
 const app = express();
 
 const Port = process.env.PORT || 3001;
