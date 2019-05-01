c63aa9b1563cfc79a4c3da4ff0c374ed5da81128
zander dyer <41213665+zandermd@users.noreply.github.com>
2019-04-30 17:05:38 -0700

    update w master

--- client/src/components/favorite/favorite.js
+++ client/src/components/favorite/favorite.js
@@ -1,27 +1,9 @@
-import React from "react";
-import { library } from '@fortawesome/fontawesome-svg-core'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-class MysteryComponent extends React.Component {
-    state = {
-      className: 'heart'
-    }
-  
-    toggleClass = (e) => {
-      if (this.state.className === 'heart') {
-        this.setState({ className: 'clicked' })
-      } else if (this.state.className === 'clicked') {
-        this.setState({ className: 'heart' })
-      }
-    }
-  
-    render() {
-      return (
-        <div className={this.state.className}>
-          <FontAwesomeIcon onClick={this.toggleClass} size={"sm"} />
-        </div>
-      )
-    }
-  }
 
-  export default MysteryComponent
+export const Heart = () => (
+  <div>
+    Favorite Drink: <FontAwesomeIcon icon="heart" />
+  </div>
+)
   
